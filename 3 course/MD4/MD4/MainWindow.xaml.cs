8dbf4f33880f89b6d773fc58a8530433ef6bf89f
Mihuil1124 <40023826+Mihuil1124@users.noreply.github.com>
2020-04-11 18:21:49 +0300

    Add files via upload

---
+++ 3 course/MD4/MD4/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MD4
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void button_Click(object sender, RoutedEventArgs e)
+		{
+			if (md4.IsChecked == true)
+			{
+				if (this.inputPanel.Text.Length != 0)
+				{
+					EnityMD4 createrHashCode = new EnityMD4();
+					this.outputPanel.Text = createrHashCode.GetHexHashFromString(this.inputPanel.Text);
+				}
+			} else 
+			{
+				if (this.inputPanel.Text.Length != 0)
+				{
+					this.outputPanel.Text = EntityMD5.Calculate(this.inputPanel.Text);
+				}
+			}
+
+			
+		}
+	}
+}
